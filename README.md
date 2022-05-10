79e6c7f308bc0cac2516f713e66325217388742d
nihal-j <nihal.b.jain@gmail.com>
2022-05-10 03:58:34 +0000

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## 3D Pose Estimation Using 2D Supervision
+
+To run the code, modify `main.py` by putting the required options inside `Args`. Then simply run `python main.py`. This will run each file with the default settings used to produce the reported results.
--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 class Args:
     dataset = 'human36m'
+    method = 'test_time_adapt' # or 'train' or 'viz'
 
 
 def main():
     
-#     args = Args()
-#     if args.dataset == 'human36m':
-#         from human36m.run import run
-#     elif args.dataset == 'humaneva':
-#         from humaneva.run import run
-        
-#     run()
-    from human36m.viz_gen import main as m
+    args = Args()
+    if args.dataset == 'human36m':
+        if args.method == 'test_time_adapt':
+            from human36m.test_time_adapt import main as m
+        elif args.method == 'train':
+            from human36m.run import run as m
+        elif args.method == 'viz':
+            from human36m.viz_gen import main as m
+            
+    elif args.dataset == 'humaneva':
+        if args.method == 'test_time_adapt':
+            from humaneva.test_time_adapt import main as m
+        elif args.method == 'train':
+            from humaneva.run import run as m
+        elif args.method == 'viz':
+            from humaneva.viz_gen import main as m
+    
     m()
-    
-    # from human36m.test_time_adapt import main as m
-    # m()
-    
 
 if __name__ == '__main__':
     main()
